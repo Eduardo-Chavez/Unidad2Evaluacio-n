5e54874fe4205afef4a8c396d3ceace282c4e07d
Eduardo-Chavez <55851434+Eduardo-Chavez@users.noreply.github.com>
2020-02-20 15:16:10 -0600

    Add files via upload

---
+++ longitud de una cadena.py
@@ -0,0 +1,2 @@
+palabra = input("Escribe tu palabra: ")
+print ("La longitud de la palabra es de: " + str(len( palabra )) )
---
+++ raiz_cuadrada.py
@@ -0,0 +1,6 @@
+import math
+
+print("Escribe un numero: ")
+numero = float(input())
+print ("Su raíz es")
+print (math.sqrt(numero))
